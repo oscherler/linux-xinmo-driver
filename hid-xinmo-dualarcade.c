5e4aacb91816274eaab44453a4e2837917a9574b
Olivier <oscherler@femto-byte.com>
2013-06-16 23:44:33 +0200

    Remove old report fixup.

--- hid-xinmo-dualarcade.c
+++ hid-xinmo-dualarcade.c
@@ -22,31 +22,12 @@
 
 #include "hid-ids.h"
 
-static __u8 *xinmo_report_fixup(struct hid_device *hdev, __u8 *rdesc,
-		unsigned int *rsize)
-{
-	if (*rsize == 112 && rdesc[16] == 0x15 && rdesc[17] == 0xff
-			&& rdesc[72] == 0x15 && rdesc[73] == 0xff) {
-
-		hid_info(hdev, "Fixing up Xin-Mo Dual Arcade report descriptor\n");
-
-		/* change Joystick 1 X and Y axes Logical Minimum to -2 */
-		rdesc[17] = 0xfe;
-
-		/* change Joystick 2 X and Y axes Logical Minimum to -2 */
-		rdesc[73] = 0xfe;
-	}
-	return rdesc;
-}
-
 /*
  * Fix negative events that are out of bounds.
  */
 static int xinmo_event(struct hid_device *hdev, struct hid_field *field,
 		struct hid_usage *usage, __s32 value)
 {
-	//struct xinmo_drv_data *drv_data = (struct lg_drv_data *)hid_get_drvdata(hdev);
-
 	switch(usage->code) {
 		case ABS_X:
 		case ABS_Y:
@@ -73,7 +54,6 @@ MODULE_DEVICE_TABLE(hid, xinmo_devices);
 static struct hid_driver xinmo_driver = {
 	.name = "xinmo",
 	.id_table = xinmo_devices,
-	//.report_fixup = xinmo_report_fixup
 	.event = xinmo_event
 };
 
